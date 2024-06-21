033c1ab4696c0644a5e5648627fa760516c7cf17
Juan Salazar <ferthelet@gmail.com>
2024-06-21 16:58:50 -0500

    agrega bucle hasta acertar

--- 2034-logica-programacion-1-Aula1/app.js
+++ 2034-logica-programacion-1-Aula1/app.js
@@ -1,19 +1,25 @@
 // variables
 let numeroSecreto = 5;
-let numeroUsuario = prompt("Me indicas un numero entre 1 y 10 por favor:");
+let numeroUsuario = 0;
 
-console.log(numeroUsuario);
+// repetir hasta que sea correcto
+while (numeroUsuario != numeroSecreto) {
 
-/* Este codigo realiza la comparacion
-*/
-if (numeroUsuario == numeroSecreto) {
-    // acertamos, insertando variable, puede ser codigo JS !!
-    alert(`Acertaste el numero es ${numeroSecreto}`);
-} else {
-    // no acertamos
-    if (numeroUsuario > numeroSecreto) {
-        alert(`El numero secreto es menor`);
+    numeroUsuario = prompt("Me indicas un numero entre 1 y 10 por favor:");
+
+    console.log(numeroUsuario);
+
+    /* Este codigo realiza la comparacion
+    */
+    if (numeroUsuario == numeroSecreto) {
+        // acertamos, insertando variable, puede ser codigo JS !!
+        alert(`Acertaste el numero es ${numeroSecreto}`);
     } else {
-        alert(`El numero secreto es mayor`);
+        // no acertamos
+        if (numeroUsuario > numeroSecreto) {
+            alert(`El numero secreto es menor`);
+        } else {
+            alert(`El numero secreto es mayor`);
+        }
     }
 }
