b0cd70ab38f2a4b72808603c0242aa9e097941be
Juan Salazar <ferthelet@gmail.com>
2024-06-21 17:40:28 -0500

    agrega contador hasta y desde 10

---
+++ 86005-desafios/app.js
@@ -0,0 +1,23 @@
+// Desafíos:
+
+// Crea un contador que comience en 1 y vaya hasta 10 usando un bucle 'while'. Muestra cada número.
+let contador = 1;
+
+while (contador <= 10) {
+    console.log(contador);
+    contador += 1;
+}
+
+// Crea un contador que comience en 10 y vaya hasta 0 usando un bucle 'while'. Muestra cada número.
+contador = 10;
+
+while (contador >= 0) {
+    console.log(contador);
+    contador -= 1;
+}
+
+// Crea un programa de cuenta progresiva. Pide un número y cuenta desde 0 hasta ese número utilizando
+// un bucle 'while' en la consola del navegador.
+
+// Crea un programa de cuenta progresiva. Pide un número y cuenta desde 0 hasta ese número utilizando
+// un bucle 'while' en la consola del navegador.
