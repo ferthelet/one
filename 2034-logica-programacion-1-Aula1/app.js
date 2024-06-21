bd89f40b4fec1cfb06cc9221caf279c4f918bfa7
Juan Salazar <ferthelet@gmail.com>
2024-06-21 14:06:13 -0500

    agragando comentarios

--- 2034-logica-programacion-1-Aula1/app.js
+++ 2034-logica-programacion-1-Aula1/app.js
@@ -1,8 +1,15 @@
+// variables
 let numeroSecreto = 6;
-let numeroUsuario = prompt("Me indicas un número por favor:");
+let numeroUsuario = prompt("Me indicas un numero por favor:");
 
 console.log(numeroUsuario);
 
+/* Este codigo realiza la comparacion
+*/
 if (numeroUsuario == numeroSecreto) {
-    alert('Acertaste el número');
+    // acertamos
+    alert('Acertaste el numero');
+} else {
+    // no acertamos
+    alert('Lo siento, no acertaste');
 }
