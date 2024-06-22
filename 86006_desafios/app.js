e880a1c00134c8c40e88b6cd3842cdc66897a8c4
Juan Salazar <ferthelet@gmail.com>
2024-06-22 10:35:26 -0500

    agrega saludos y resultados por console y alert

--- 86006_desafios/app.js
+++ 86006_desafios/app.js
@@ -1,20 +1,31 @@
 // Desafíos finales:
 
 // Crea un programa que utilice console.log para mostrar un mensaje de bienvenida.
+console.log("Bienvenido/a");
 
 // Crea una variable llamada "nombre" y asígnale tu nombre. Luego, utiliza console.log para mostrar
 // el mensaje "¡Hola, [tu nombre]!" en la consola del navegador.
+let nombre = "Juan";
+console.log("Hola " + nombre);
 
 // Crea una variable llamada "nombre" y asígnale tu nombre. Luego, utiliza alert para mostrar el
 // mensaje "¡Hola, [tu nombre]!".
+nombre = "Juan";
+alert(`Hola ${nombre}`);
 
 // Utiliza prompt y haz la siguiente pregunta: ¿Cuál es el lenguaje de programación que más te
 // gusta?. Luego, almacena la respuesta en una variable y muestra la respuesta en la consola del navegador.
+let respuesta = prompt("¿Cuál es el lenguaje de programación que más te gusta?");
+console.log(respuesta);
 
 // Crea una variable llamada "valor1" y otra llamada "valor2", asignándoles valores numéricos de tu
 // elección. Luego, realiza la suma de estos dos valores y almacena el resultado en una tercera variable
 // llamada "resultado". Utiliza console.log para mostrar el mensaje "La suma de [valor1] y [valor2] es
 // igual a [resultado]." en la consola.
+let valor1 = 5;
+let valor2 = 7;
+let resultado = valor1 + valor2;
+console.log(`La suma de ${valor1} y ${valor2} es igual a ${resultado}.`)
 
 // Crea una variable llamada "valor1" y otra llamada "valor2", asignándoles valores numéricos de tu
 // elección. Luego, realiza la resta de estos dos valores y almacena el resultado en una tercera variable
