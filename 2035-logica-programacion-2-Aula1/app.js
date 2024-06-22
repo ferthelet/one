ebb0c165030b4100c4ad78417714df4efe4c51b3
Juan Salazar <ferthelet@gmail.com>
2024-06-22 17:39:00 -0500

    agregar texto con querySelector h1 y p

---
+++ 2035-logica-programacion-2-Aula1/app.js
@@ -0,0 +1,5 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Juego del numero secreto';
+
+let parrafo = document.querySelector('p');
+parrafo.innerHTML = 'Ingresa un numero del 1 al 10';
