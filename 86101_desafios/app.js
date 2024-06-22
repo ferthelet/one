7bd099fcc4051587c61e32792bff36f577362afc
Juan Salazar <ferthelet@gmail.com>
2024-06-22 18:42:59 -0500

    agrega ciudad, amo JS y suma

--- 86101_desafios/app.js
+++ 86101_desafios/app.js
@@ -16,8 +16,20 @@ function clickAlert() {
 // Crea una función que se ejecute cuando se haga clic en el botón "prompt", preguntando
 // el nombre de una ciudad de Brasil. Luego, muestra una alerta con el mensaje concatenando
 // la respuesta con el texto: "Estuve en {ciudad} y me acordé de ti".
+function preguntaCiudad() {
+    let ciudad = prompt("Ingrese una ciudad");
+    alert(`Estuve en ${ciudad} y me acordé de ti`);
+}
 
 // Crea una función que muestre una alerta con el mensaje: "Yo amo JS" siempre que se
 // presione el botón "Alerta".
+function amoJS() {
+    alert("Yo amo JS ❤️");
+}
 
 // Al hacer clic en el botón "suma", pide 2 números y muestra el resultado de la suma en una alerta.
+function sumandos() {
+    let sumando1 = parseInt(prompt("Ingresa el primer numero"));
+    let sumando2 = parseInt(prompt("Ingresa el segundo numero"));
+    alert(`La suma de ${sumando1} y ${sumando2} es ${sumando1 + sumando2}`)
+}
