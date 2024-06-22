24b102bc3609620065b93d2091afe22768760523
Juan Salazar <ferthelet@gmail.com>
2024-06-22 17:50:22 -0500

    agrega evento click y fn

--- 2035-logica-programacion-2-Aula1/app.js
+++ 2035-logica-programacion-2-Aula1/app.js
@@ -3,3 +3,7 @@ titulo.innerHTML = 'Juego del numero secreto';
 
 let parrafo = document.querySelector('p');
 parrafo.innerHTML = 'Ingresa un numero del 1 al 10';
+
+function intentoDeUsuario() {
+    alert('Click desde boton');
+}
