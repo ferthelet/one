dc9728bea33dbc95d332443fe6e6dd4bfb6ae188
Juan Salazar <ferthelet@gmail.com>
2024-06-22 19:51:22 -0500

    agrega asignacion texto elemento generico

--- 2035-logica-programacion-2-Aula1/app.js
+++ 2035-logica-programacion-2-Aula1/app.js
@@ -1,9 +1,16 @@
-let titulo = document.querySelector('h1');
-titulo.innerHTML = 'Juego del numero secreto';
+// let titulo = document.querySelector('h1');
+// titulo.innerHTML = 'Juego del numero secreto';
+asignarTextoElemento('h1', "Juego del numero secreto")
 
-let parrafo = document.querySelector('p');
-parrafo.innerHTML = 'Ingresa un numero del 1 al 10';
+// let parrafo = document.querySelector('p');
+// parrafo.innerHTML = 'Ingresa un numero del 1 al 10';
+asignarTextoElemento('p', "Ingresa un numero del 1 al 10");
 
 function intentoDeUsuario() {
     alert('Click desde boton');
+}
+
+function asignarTextoElemento(elemento, texto) {
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
 }
