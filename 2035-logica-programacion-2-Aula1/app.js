b47018aa75d3fd1891b329eebe15e156b7eb24d6
Juan Salazar <ferthelet@gmail.com>
2024-06-22 20:09:07 -0500

    sube funciones, agrega genera secreto

--- 2035-logica-programacion-2-Aula1/app.js
+++ 2035-logica-programacion-2-Aula1/app.js
@@ -1,16 +1,28 @@
+// hoisting probado en version anterior
+// redordenando funciones on top
+
+const NUMERO_MAX = 10;
+let numeroSecreto = generaSecreto(NUMERO_MAX);
+console.log(numeroSecreto); // TBD
+
+function asignarTextoElemento(elemento, texto) {
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+}
+
+function intentoDeUsuario() {
+    alert('Click desde boton');
+}
+
+function generaSecreto(max) {
+    return Math.floor(Math.random() * max) + 1;    
+}
+
 // let titulo = document.querySelector('h1');
 // titulo.innerHTML = 'Juego del numero secreto';
 asignarTextoElemento('h1', "Juego del numero secreto")
 
 // let parrafo = document.querySelector('p');
 // parrafo.innerHTML = 'Ingresa un numero del 1 al 10';
-asignarTextoElemento('p', "Ingresa un numero del 1 al 10");
+asignarTextoElemento('p', `Ingresa un numero del 1 al ${NUMERO_MAX}`);
 
-function intentoDeUsuario() {
-    alert('Click desde boton');
-}
-
-function asignarTextoElemento(elemento, texto) {
-    let elementoHTML = document.querySelector(elemento);
-    elementoHTML.innerHTML = texto;
-}
