812a4590e19f2c25aed226b543a61cfd51e0e160
Juan Salazar <ferthelet@gmail.com>
2024-06-24 05:41:38 -0500

    commit inicial, agrega frutas al array

---
+++ 86064_mdn_array/app.js
@@ -0,0 +1,8 @@
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
+
+const fruits = [];
+fruits.push("banana", "apple", "peach");
+console.log(fruits.length);
+
+fruits[5] = "mango";
+console.log(fruits.length);
