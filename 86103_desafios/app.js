c764d2c0e8eaab7ad4e2efb707de89f63f5062c6
Juan Salazar <ferthelet@gmail.com>
2024-06-23 21:24:56 -0500

    agrega imc y factorial

--- 86103_desafios/app.js
+++ 86103_desafios/app.js
@@ -1,8 +1,23 @@
 // Desafíos:
 // Crea una función que calcule el índice de masa corporal (IMC) de una persona a partir de
 // su altura en metros y peso en kilogramos, que se recibirán como parámetros.
+function calculaIMC(altura, peso) {
+    return peso / altura ** 2; // ** exponent
+}
+console.log(calculaIMC(1.65, 70));
 
 // Crea una función que calcule el valor del factorial de un número pasado como parámetro.
+function factorial(numero) {
+    let acumulador = 1;
+
+    while (numero > 0) {
+        acumulador *= numero;
+        numero--;
+    }
+    return acumulador;
+}
+
+console.log(factorial(2));
 
 // Crea una función que convierta un valor en dólares, pasado como parámetro, y devuelva el
 // valor equivalente en reales(moneda brasileña,si deseas puedes hacerlo con el valor del
