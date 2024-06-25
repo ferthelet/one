ec5d82dda06914ff7dc0a19ace42688a299fd7a1
Juan Salazar <ferthelet@gmail.com>
2024-06-24 21:14:38 -0500

    agrega primeros cuatro desafios

--- 86104_desafios/app.js
+++ 86104_desafios/app.js
@@ -1,13 +1,23 @@
 // Desafíos
 // Crea una lista vacía llamada "listaGenerica".
+let listaGenerica = [];
 
 // Crea una lista de lenguajes de programación llamada "lenguagesDeProgramacion con los
 //  siguientes elementos: 'JavaScript', 'C', 'C++', 'Kotlin' y 'Python'.
+let lenguagesDeProgramacion = ['JavaScript', 'C', 'C++', 'Kotlin', 'Python'];
 
 // Agrega a la lista "lenguagesDeProgramacion los siguientes elementos: 'Java', 'Ruby'
 // y 'GoLang'.
+lenguagesDeProgramacion.push('Java', 'Ruby', 'GoLang');
+
 // Crea una función que muestre en la consola todos los elementos de la lista
 // "lenguagesDeProgramacion.
+function mostrarElementos(lista) {
+    for (let i = 0; i < lista.length; i++) {
+        console.log(lista[i]);
+    }
+}
+mostrarElementos(lenguagesDeProgramacion);
 
 // Crea una función que muestre en la consola todos los elementos de la lista
 // "lenguagesDeProgramacion en orden inverso.
