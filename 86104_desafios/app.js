1cbd316ef60d29b53cac6cfee0ffefe82ae17866
Juan Salazar <ferthelet@gmail.com>
2024-06-24 21:19:56 -0500

    agrega lista invertida, promedio, min/max, suma

--- 86104_desafios/app.js
+++ 86104_desafios/app.js
@@ -21,11 +21,52 @@ mostrarElementos(lenguagesDeProgramacion);
 
 // Crea una función que muestre en la consola todos los elementos de la lista
 // "lenguagesDeProgramacion en orden inverso.
+function mostrarElementosInverso(lista) {
+    for (let i = lista.length - 1; i >= 0; i--) {
+        console.log(lista[i]);
+    }
+}
+mostrarElementosInverso(lenguagesDeProgramacion);
 
 // Crea una función que calcule el promedio de los elementos en una lista de números.
+function calcularPromedio(lista) {
+    let suma = 0;
+    for (let i = 0; i < lista.length; i++) {
+        suma += lista[i];
+    }
+    return suma / lista.length;
+}
+
+let numeros = [1, 2, 3, 4, 5];
+let promedio = calcularPromedio(numeros);
 
 // Crea una función que muestre en la consola el número más grande y el número más pequeño
 // en una lista.
+function mostrarMinMax(lista) {
+    let min = lista[0];
+    let max = lista[0];
+    for (let i = 1; i < lista.length; i++) {
+        if (lista[i] < min) {
+            min = lista[i];
+        }
+        if (lista[i] > max) {
+            max = lista[i];
+        }
+    }
+    console.log(`Mínimo: ${min}`);
+    console.log(`Máximo: ${max}`);
+}
+mostrarMinMax(numeros);
+
+// Crea una función que muestre en la consola la suma de todos los elementos en una lista.
+function sumarElementos(lista) {
+    let suma = 0;
+    for (let i = 0; i < lista.length; i++) {
+        suma += lista[i];
+    }
+    console.log(`Suma: ${suma}`);
+}
+sumarElementos(numeros);
 
 // Crea una función que devuelva la suma de todos los elementos en una lista.
 
