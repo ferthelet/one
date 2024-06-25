649759946d64adc2942d688feb515561e45649ca
Juan Salazar <ferthelet@gmail.com>
2024-06-24 21:27:18 -0500

    corrige promedio, agrega muestra y retorna suma

--- 86104_desafios/app.js
+++ 86104_desafios/app.js
@@ -34,11 +34,11 @@ function calcularPromedio(lista) {
     for (let i = 0; i < lista.length; i++) {
         suma += lista[i];
     }
-    return suma / lista.length;
+    console.log(`Promedio ${suma / lista.length}`);
 }
 
 let numeros = [1, 2, 3, 4, 5];
-let promedio = calcularPromedio(numeros);
+calcularPromedio(numeros);
 
 // Crea una función que muestre en la consola el número más grande y el número más pequeño
 // en una lista.
@@ -69,6 +69,14 @@ function sumarElementos(lista) {
 sumarElementos(numeros);
 
 // Crea una función que devuelva la suma de todos los elementos en una lista.
+function sumarElementos(lista) {
+    let suma = 0;
+    for (let i = 0; i < lista.length; i++) {
+        suma += lista[i];
+    }
+    return suma;
+}
+console.log(sumarElementos(numeros));
 
 // Crea una función que devuelva la posición en la lista donde se encuentra un elemento
 // pasado como parámetro, o -1 si no existe en la lista.
